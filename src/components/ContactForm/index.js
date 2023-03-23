5640213a057a933e38d6e32f1ad7d18b72312495
adrianomolin <adriano.molin@hotmail.com>
2023-03-23 11:37:39 -0300

    feat: form validation and error handling

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,32 +1,88 @@
 import PropTypes from 'prop-types';
-import FormGroup from '../FormGroup';
+import { useState } from 'react';
+
+import isEmailValid from '../../utils/isEmailValid';
 
 import { ButtonContainer, Form } from './styles';
 
 import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
+import FormGroup from '../FormGroup';
+import { useErrors } from '../../hooks/useErrors';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  const { setError, removeError, getErrorMessageByFieldName } = useErrors();
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: 'name', message: 'Nome é obrigatório' });
+    } else {
+      removeError('name');
+    }
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      setError({ field: 'email', message: 'E-mail inválido.' });
+    } else {
+      removeError('email');
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({
+      name, email, phone, category,
+    });
+  }
+
   return (
-    <Form>
-      <FormGroup>
-        <Input placeholder="Nome" />
+    <Form onSubmit={handleSubmit}>
+      <FormGroup error={getErrorMessageByFieldName('name')}>
+        <Input
+          placeholder="Nome"
+          value={name}
+          onChange={handleNameChange}
+          error={getErrorMessageByFieldName('name')}
+        />
       </FormGroup>
 
-      <FormGroup
-        error="O formato do e-mail é inválido"
-      >
-        <Input placeholder="E-mail" error />
+      <FormGroup error={getErrorMessageByFieldName('email')}>
+        <Input
+          placeholder="E-mail"
+          value={email}
+          onChange={handleEmailChange}
+          error={getErrorMessageByFieldName('email')}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="">Categoria</option>
           <option value="instagram">Instagram</option>
+          <option value="discord">Discord</option>
         </Select>
       </FormGroup>
 
